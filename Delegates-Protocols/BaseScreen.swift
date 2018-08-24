2c5d632be7809dc6715c68f01b93accbf694855b
dennisvera <dennisvera.z@gmail.com>
2018-08-24 10:56:18 -0400

    Delegates and Protocols inplementation.

--- Delegates-Protocols/BaseScreen.swift
+++ Delegates-Protocols/BaseScreen.swift
@@ -9,27 +9,36 @@
 import UIKit
 
 class BaseScreen: UIViewController {
+    
+    // MARK: - Outlets
 
+    @IBOutlet weak var mainImageView: UIImageView!
+    @IBOutlet weak var chooseButton: UIButton!
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    // MARK: - View Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        chooseButton.layer.cornerRadius = chooseButton.frame.size.height / 2
     }
     
+    // MARK: - Actions
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func chooseButtonTapped(_ sender: UIButton) {
+        let selectionVC = storyboard?.instantiateViewController(withIdentifier: "SelectionScreen") as! SelectionScreen
+        selectionVC.selectionDelegate = self
+        present(selectionVC, animated: true, completion: nil)
     }
-    */
+}
 
+// MARK: - Extesnion
+
+extension BaseScreen: SideSelectionDelegate {
+    
+    func didTapChoice(image: UIImage, name: String, color: UIColor) {
+        mainImageView.image = image
+        nameLabel.text = name
+        view.backgroundColor = color
+    }
 }
--- Delegates-Protocols/SelectionScreen.swift
+++ Delegates-Protocols/SelectionScreen.swift
@@ -8,28 +8,35 @@
 
 import UIKit
 
+// MARK: - Prototcol
+
+protocol SideSelectionDelegate {
+    
+    func didTapChoice(image: UIImage, name: String, color: UIColor)
+}
+
 class SelectionScreen: UIViewController {
+    
+    // MARK: - Properties
+    
+    var selectionDelegate: SideSelectionDelegate!
+    
+    // MARK: - View Lifecycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
+    // MARK: - Actions
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func imperialButtonTapped(_ sender: UIButton) {
+        selectionDelegate.didTapChoice(image: UIImage(named: "vader")!, name: "Darth Vader", color: .red)
+        dismiss(animated: true, completion: nil)
     }
-    */
 
+    @IBAction func rebelButtonTapped(_ sender: UIButton) {
+        selectionDelegate.didTapChoice(image: UIImage(named: "luke")!, name: "Luke Skywalker", color: .cyan)
+        dismiss(animated: true, completion: nil)
+    }
+    
 }
